d2358523ca13ba2b084509f1879ab6f975bb19db
rjvak7 <khanna.rajiv84@gmail.com>
2016-12-07 15:47:00 -0600

    first commit

---
+++ Helper.py
@@ -0,0 +1,28 @@
+# maintained by rajivak@utexas.edu
+import numpy as np
+
+def format_numsel(numsel):
+    ss = ''
+    for i,j in enumerate(numsel):
+        ss = ss + " %d:%d " %(i,j)
+    return ss
+
+def get_train_testindices(n, ntest, seed):
+    np.random.seed(seed)
+    testindices = np.random.choice(n,ntest,replace=False)
+    trainindices = np.setdiff1d( range(n), testindices)
+    return trainindices, testindices
+
+def exit(str):
+    print str
+    exit(1)
+
+
+def dir_exists(filename):
+    import os
+    dir = os.path.dirname(filename)
+
+    try:
+        os.stat(dir)
+    except:
+        os.mkdir(dir)
